90f584b90c10cf7c7ee1310c6a17d0e2686b47a8
priyanshptl <61774794+priyanshptl@users.noreply.github.com>
2021-10-17 19:19:08 +0530

    Update nav.php

--- Partial/nav.php
+++ Partial/nav.php
@@ -5,8 +5,12 @@ if(isset($_SESSION['email']) && $_SESSION['email']==true){
 else{
   $email = false;
 }
-echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/Priyansh/home.php">CRUD App</a>
+echo '';
+
+      if(!$email){
+      echo '
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/Priyansh/login.php">CRUD App</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -14,16 +18,26 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/Priyansh/home.php">Home <span class="sr-only">(current)</span></a>
-      </li>';
-
-      if(!$email){
-      echo '<li class="nav-item">
+        <a class="nav-link" href="/Priyansh/login.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="/Priyansh/login.php">Login</a>
       </li>';
       }
       if($email){
-      echo '<li class="nav-item">
+      echo '
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/Priyansh/index.php">CRUD App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/Priyansh/index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
         <a class="nav-link" href="/Priyansh/logout.php">Logout</a>
       </li>';
     }
